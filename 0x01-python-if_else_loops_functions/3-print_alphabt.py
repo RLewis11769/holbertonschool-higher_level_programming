8dc8f6aea38eca944be09be73e139727ac60ede9
Rachel Lewis <RLewis11769@gmail.com>
2021-04-28 04:57:47 +0000

    == and continue instead

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for alph in range(97, 123):
-    if alph != 101 and alph != 113:
-         print("{}".format(chr(alph)), end='')
+    if alph == 101 and alph == 113:
+        continue
+    print("{}".format(chr(alph)), end='')
