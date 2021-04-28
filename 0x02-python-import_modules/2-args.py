68653df9783c87d2d40a8f6a76a61d1936a59069
Rachel Lewis <RLewis11769@gmail.com>
2021-04-28 01:11:46 +0000

    Add : for correct statement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if num == 1:
         print("0 arguments.")
     elif num == 2:
-        print("{} argument".format(num - 1))
+        print("{} argument:".format(num - 1))
     else:
         print("{} arguments:".format(num - 1))
     for elem in range(1, num):
