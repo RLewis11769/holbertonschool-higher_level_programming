6218c7cc4b8dbe581cd9658f0ce105cf2f888ba4
Rachel Lewis <RLewis11769@gmail.com>
2021-04-28 04:24:16 +0000

    Modify syntax

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,14 +8,13 @@ if __name__ == "__main__":
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        operator = sys.argv[2]
-        if operator == '+':
+        if sys.argv[2] == "+":
             print("{} + {} = {}".format(a, b, add(a, b)))
-        elif operator == '-':
+        elif sys.argv[2] == "-":
             print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif operator == '*':
+        elif sys.argv[2] == "*":
             print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif operator == '/':
+        elif sys.argv[2] == "/":
             print("{} / {} = {}".format(a, b, div(a, b)))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
