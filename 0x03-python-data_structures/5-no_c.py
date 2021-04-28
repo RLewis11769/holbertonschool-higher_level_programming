ded341837d1424939785ccba6896424028ada248
Rachel Lewis <RLewis11769@gmail.com>
2021-04-28 22:51:59 +0000

    Remove null check to print nothing

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        new = ""
-        for idx in my_string:
-            if idx == 'c' or idx == 'C':
-                continue
-            new = new + idx
-        return new
+    new = ""
+    for idx in my_string:
+        if idx == 'c' or idx == 'C':
+            continue
+        new = new + idx
+    return new
