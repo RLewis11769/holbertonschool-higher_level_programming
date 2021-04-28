c5a886b382ea15cf196d9b6a919d442b2df43c00
Rachel Lewis <RLewis11769@gmail.com>
2021-04-28 01:10:07 +0000

    Add elif for 1 argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     num = len(sys.argv)
     if num == 1:
         print("0 arguments.")
+    elif num == 2:
+        print("{} argument".format(num - 1))
     else:
         print("{} arguments:".format(num - 1))
-        for elem in range(1, num):
-            print("{}: {}".format(elem, sys.argv[elem]))
+    for elem in range(1, num):
+        print("{}: {}".format(elem, sys.argv[elem]))
