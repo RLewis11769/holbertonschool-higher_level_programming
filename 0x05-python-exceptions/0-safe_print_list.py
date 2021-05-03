f6a96cd81c35cf4a70535532618e2b7c4099097b
Rachel Lewis <RLewis11769@gmail.com>
2021-05-03 18:50:42 +0000

    Add data to 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+"""
+safe_print_list - prints x number of a list
+my_list - list of any type
+x - number of elements to print
+Returns: number of elements printed
+"""
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for val in range(x):
+            print("{}".format(my_list[val]), end="")
+            count += 1
+        print()
+        return count
+    except:
+        print()
+        return count
