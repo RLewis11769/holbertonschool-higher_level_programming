b59e071acbc252aa8dc2116a942b7b01d8a8556d
Rachel Lewis <RLewis11769@gmail.com>
2021-05-03 19:04:27 +0000

    Add data to 1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+"""
+safe_print_integer - prints integer only
+value - input of any type
+Returns: True if integer
+"""
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
