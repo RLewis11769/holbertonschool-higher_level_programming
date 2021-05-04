a6dde7f0b324b7322d21012e00107f1e9f13e453
Rachel Lewis <RLewis11769@gmail.com>
2021-05-04 14:58:13 +0000

    Attempt proper documentation

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,7 @@ safe_print_list - prints x number of a list
 @x - number of elements to print
 Returns: number of elements printed
 """
+
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
