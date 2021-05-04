2c5a94d33e7bf527e0b648afec009ed35d3f859d
Rachel Lewis <RLewis11769@gmail.com>
2021-05-04 15:09:05 +0000

    Add documentation for module, class, and method

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+""" Module that defines square with private attribute """
+
+
 class Square:
+    """ Defines class with size attribute for each instance """
     def __init__(self, size):
+        """ Method that defines size for each instance """
         self.__size = size
