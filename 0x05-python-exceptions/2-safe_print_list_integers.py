42ebe2df8f9288526de3ab6647bcab26be807621
Rachel Lewis <RLewis11769@gmail.com>
2021-05-04 14:23:12 +0000

    Remove null check

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,14 +7,13 @@ x - number of elements to print
 Returns: number of integers printed
 """
 def safe_print_list_integers(my_list=[], x=0):
-    if my_list:
-        count = 0
-        for val in range(x):
-            try:
-                if isinstance(val, int):
-                    print("{:d}".format(my_list[val]), end="")
-                    count += 1
-            except (TypeError, ValueError):
-                print("", end="")
-        print()
-        return count
+    count = 0
+    for val in range(x):
+        try:
+            if isinstance(val, int):
+                print("{:d}".format(my_list[val]), end="")
+                count += 1
+        except (TypeError, ValueError):
+            print("", end="")
+    print()
+    return count
