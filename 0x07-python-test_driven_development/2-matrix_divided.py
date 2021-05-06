45082253cdeb47fda57f1a2d27c41c0e6aafa14b
Rachel Lewis <RLewis11769@gmail.com>
2021-05-06 03:08:37 +0000

    Add data to 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,19 +11,32 @@ Return: new matrix containing results of division
 def matrix_divided(matrix, div):
     """ Divides contents of matrix by div """
 
-    # tests matrix and elements in matrix
+    """ Tests matrix and elements in matrix """
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    for elements in matrix:
-        if type(elements) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError
+        ("matrix must be a matrix (list of lists) of integers/floats")
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError
+            ("matrix must be a matrix (list of lists) of integers/floats")
+        for num in row:
+            if not isinstance(num, (int, float)):
+                raise TypeError
+                ("matrix must be a matrix (list of lists) of integers/floats")
 
-    # test div
+    """ Tests length of matrix """
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+
+    """ Tests div is proper number """
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    # return matrix divided by div as seen in 100-
-    new_matrix = list(map(lambda num: list(map(lambda num: round(num / div, 2), num)), matrix))
+    """ return matrix divided by div as seen in 0x04-101 """
+    new_matrix = list(map(lambda num:
+                          list(map(lambda num:
+                                   round(num / div, 2), num)), matrix))
     return new_matrix
