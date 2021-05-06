76eabcae80035942dd556ec9a2bae5123da5c981
Rachel Lewis <RLewis11769@gmail.com>
2021-05-06 03:32:40 +0000

    Add data to 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+say_my_name - prints name
+@first_name: first name string
+@last_name: last name string
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
