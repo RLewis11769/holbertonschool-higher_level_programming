6c08f018510cdfdf71af028ee6bc39745975b8ed
Rachel Lewis <RLewis11769@gmail.com>
2021-05-07 03:19:46 +0000

    Add data to 100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+safe_print_integer - prints integer
+@value: any type (integers printed, others raise error)
+Return: True if integer, False if not
+"""
+
+import sys
+
+def safe_print_integer_err(value):
+    """ Regardless of input, prints integers not other types """
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        """ if exception, prints message from stderr to stdout """
+        print("Exception: {}".format(e), file=sys.stderr)
