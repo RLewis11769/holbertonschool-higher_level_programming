bfe77fb993bea782b7a152fefd2eef98aef916ab
Rachel Lewis <RLewis11769@gmail.com>
2021-05-07 21:12:09 +0000

    Add documentation and final test

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,6 +8,7 @@ say_my_name - prints name
 
 
 def say_my_name(first_name, last_name=""):
+    """ Function to print introduction when passed name """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
