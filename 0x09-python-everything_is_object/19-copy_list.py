b36a316e44c4bb569e7cd30e050c5fe60c98e16e
Rachel Lewis <RLewis11769@gmail.com>
2021-05-10 20:41:29 +0000

    Remove documentation to make function exactly 3 lines

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
-"""
-copy_list - copies one list into another without aliasing
-@l: list to copy into new list
-"""
-
-
 def copy_list(l):
-    """ copies l into new list (not specified with new variable name) """
     return l.copy()
