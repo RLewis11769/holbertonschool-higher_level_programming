5fd731b622165d077c6048e41ede544d1eb89d18
Rachel Lewis <RLewis11769@gmail.com>
2021-05-11 19:24:29 +0000

    Correct style/clean up language

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-is_kind_of_class - checks if given object is instance of class that inherits from given class
+is_kind_of_class - checks if given object is instance or inherits from class
 @obj: object to check
 @a_class: class to check if obj inherits from given class
 Return: True or False
@@ -9,5 +9,5 @@ Return: True or False
 
 
 def is_kind_of_class(obj, a_class):
-    """ Returns boolean if obj is instance of class that inherits from a_class """
+    """ Returns boolean if obj is instance of or inherits from a_class """
     return isinstance(obj, a_class)
