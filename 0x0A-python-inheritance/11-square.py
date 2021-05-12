3898c8d6f65797bc90a9767575ee3affd63f6289
Rachel Lewis <RLewis11769@gmail.com>
2021-05-12 01:43:38 +0000

    Add data to 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" Module that defines Square class """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Defines methods and attributes of Square class """
+    """ Some methods and attributes inherited from Rectangle """
+
+    def __init__(self, size):
+        """ Instantiates private size attribute """
+        self.integer_validator("size", size)
+        self.__size = size
+
+        """ Implements area method from Rectangle """
+        super().__init__(size, size)
+
+    def __str__(self):
+        """ Builtin method required "Rectangle" hardcoded in Rectangle class """
+        return "[Square] {}/{}".format(self.__size, self.__size)
