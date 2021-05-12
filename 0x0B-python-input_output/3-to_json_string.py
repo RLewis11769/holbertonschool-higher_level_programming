ddd53533f02e6fafe8b99b0b6365133738dbffee
Rachel Lewis <RLewis11769@gmail.com>
2021-05-12 19:41:21 +0000

    Add data to 3-to_jason_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+to_json_string - returns JSON representation of string
+@my_obj: object/string to find JSON representation of
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """ finds JSON representation of my_obj """
+    jsonStr = json.dumps(my_obj)
+    return jsonStr
