230c452757762439eddf1bbb13fe78fda3c4e2da
Rachel Lewis <RLewis11769@gmail.com>
2021-05-13 01:28:29 +0000

    Add data to 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+class_to_json - returns dictionary description for JSON object
+@obj: JSON object to find dict of
+Return: dictionary description for obj
+"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """  Finds dict for obj """
+    return obj.__dict__
