14a97cdd7587d4982ce85eaac40c0db3990d2928
Rachel Lewis <RLewis11769@gmail.com>
2021-05-13 05:46:02 +0000

    Better comments

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,12 +10,16 @@ append_after - inserts text to file if line contains string
 
 def append_after(filename="", search_string="", new_string=""):
     """ Appends new_string to filename after search_string line in filename """
+
     string = ""
     with open(filename) as f:
         for line in f:
+            """ Adds each line to new line """
             string += str(line)
+            """ If search_string is in line, adds string after """
             if search_string in line:
                 string += new_string
 
+    """ Overwrites file with final string """
     with open(filename, 'w') as f:
         f.write(string)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+""" Loads, adds, and saves command arguments to new file """
+
 """ Add items via command line - sys.argv[] """
 import json
 import sys
@@ -7,11 +9,13 @@ import sys
 save_to = __import__('5-save_to_json_file').save_to_json_file
 load_from = __import__('6-load_from_json_file').load_from_json_file
 
+""" Adds existing file info to new_list """
 try:
     new_list = load_from("add_item.json")
 except:
     new_list = []
 
+""" Adds command line arguments to new_list """
 for x in range(1, len(sys.argv)):
     new_list.append(sys.argv[x])
 
