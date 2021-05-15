6117298147960a0ef69088098af4dcef4e47b5eb
Rachel Lewis <RLewis11769@gmail.com>
2021-05-15 00:10:09 +0000

    Add getter/setter and validation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,9 +13,23 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ Instantiates attributes for Square (from Rectangle) """
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     def __str__(self):
         """ Builtin that produces readable output """
         return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
                                                 self.id, self.x, self.y,
                                                 self.width)
+
+    @property
+    def size(self):
+        """ Gets private size attribute """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Sets size attribute from width/height with exceptions """
+        self.int_validator("width", value)
+        self.value_validator("width", value)
+        self.width = value
+        self.height = value
