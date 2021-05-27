0556eefbb3a1925f141dd34fe37521fe81238450
Rachel Lewis <RLewis11769@gmail.com>
2021-05-27 01:24:35 +0000

    Limit at bottom instead of top at top

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,7 @@
 -- Display 3 hottest cities during July and August
-SELECT TOP 3 city, avg(value) as avg_temp
+SELECT city, avg(value) as avg_temp
 FROM temperatures
 WHERE month = 7 OR month = 8
 GROUP BY city
-ORDER BY avg_temp DESC;
+ORDER BY avg_temp DESC
+LIMIT 3;
