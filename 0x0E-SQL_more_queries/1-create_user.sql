80631c87a000bc1bb7bdbe07697f69d2fc9685c0
Rachel Lewis <RLewis11769@gmail.com>
2021-05-27 16:06:47 +0000

    Add if exists to drop

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create user with all privileges
-DROP USER 'user_0d_1'@'localhost';
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 
 CREATE USER 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
