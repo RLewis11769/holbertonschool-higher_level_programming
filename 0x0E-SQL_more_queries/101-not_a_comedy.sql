516b0f839e8770b3b73ba5ff577017ad6541281e
Rachel Lewis <RLewis11769@gmail.com>
2021-05-28 02:22:03 +0000

    Add data to 101-not_a_comedy.sql

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,13 @@
+-- Use two joins to find shows without Comedy genre
+
+SELECT title
+FROM tv_shows
+WHERE title
+NOT IN (SELECT tv_shows.title
+        FROM tv_shows
+    	INNER JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id 
+	INNER JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+	WHERE tv_genres.name = 'Comedy') 
+ORDER BY title;
