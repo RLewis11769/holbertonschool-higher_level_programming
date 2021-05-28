458a84e9f810714c5d60959e9168e2322297a635
Rachel Lewis <RLewis11769@gmail.com>
2021-05-28 01:00:17 +0000

    Keywords in all caps

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Use join to find count of shows for each genre
-SELECT tv_genres.name as genre, count(*) as number_of_shows
+SELECT tv_genres.name AS genre, count(*) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
