3fa3ed2dc256a13fb3aa2fb899efdf6d1cf077c7
Rachel Lewis <RLewis11769@gmail.com>
2021-05-28 02:36:33 +0000

    Keywords in uppercase

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- Sum tv show ratings
-SELECT tv_shows.title, sum(tv_show_ratings.rate) as rating
+SELECT tv_shows.title, sum(tv_show_ratings.rate) AS rating
 FROM tv_shows
 INNER JOIN tv_show_ratings
      ON tv_shows.id = tv_show_ratings.show_id
