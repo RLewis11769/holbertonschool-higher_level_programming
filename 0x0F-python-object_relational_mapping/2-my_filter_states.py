7b79fcffa2b913a0f7e43792291cd88cfb8aa6e2
Rachel Lewis <RLewis11769@gmail.com>
2021-05-29 01:41:09 +0000

    Add data to 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" Prints all states matching argv[4] """
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
+    data = cursor.fetchall()
+
+    for entry in data:
+        print(entry)
