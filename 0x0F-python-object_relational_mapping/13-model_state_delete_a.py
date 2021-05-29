faf097165c09cc1c5efc06bf0376aea83acd9940
Rachel Lewis <RLewis11769@gmail.com>
2021-05-29 22:37:41 +0000

    Add data to 13-model_state_delete_a.py

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""" Print all data from State object if name contains 'a' """
+
+
+if __name__ == "__main__":
+
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State)
+    for state in states:
+        if 'a' in state.name:
+            session.delete(state)
+    session.commit()
