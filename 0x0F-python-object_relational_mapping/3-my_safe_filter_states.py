9509138b83c3608360a105202ceac014f4f980cb
Rachel Lewis <RLewis11769@gmail.com>
2021-05-29 16:29:58 +0000

    Try with just a close

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" Prints all states matching argv[4] """
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+    with db.cursor() as cursor:
+        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4]))
+
+        data = cursor.fetchall()
+
+        for entry in data:
+            print(entry)
