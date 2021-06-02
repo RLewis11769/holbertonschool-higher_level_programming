2208054cfd67ceb536525e615391bdee6a0f32dc
Rachel Lewis <RLewis11769@gmail.com>
2021-06-02 20:40:51 +0000

    Add data to 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/* Print string statements argv[1] number of times */
+const args = process.argv.slice(2);
+const num = args[0];
+let x;
+let string = "";
+
+if (parseInt(num)) {
+  for (x = 0; x < num; x++) {
+    string += 'X';
+  }
+  for (x = 0; x < string.length; x++) {
+    console.log(string)
+  }
+} else {
+  console.log('Missing size')
+}
