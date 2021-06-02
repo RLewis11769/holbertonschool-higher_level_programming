333cc3e1cc6c4c7f08cf8a6a9cf51e2a4fc57e7e
Rachel Lewis <RLewis11769@gmail.com>
2021-06-02 20:42:01 +0000

    Please remember semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,15 +3,15 @@
 const args = process.argv.slice(2);
 const num = args[0];
 let x;
-let string = "";
+let string = '';
 
 if (parseInt(num)) {
   for (x = 0; x < num; x++) {
     string += 'X';
   }
   for (x = 0; x < string.length; x++) {
-    console.log(string)
+    console.log(string);
   }
 } else {
-  console.log('Missing size')
+  console.log('Missing size');
 }
