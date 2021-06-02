9cb4651c1aba6eec4776a92863832909119fa8f7
Rachel Lewis <RLewis11769@gmail.com>
2021-06-02 19:59:51 +0000

    Add data to 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* Print string statements using loop */
+let x;
+const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (x = 0; x < list.length; x++) {
+  console.log(list[x]);
+}
