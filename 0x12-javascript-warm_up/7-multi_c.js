1a6c7fe32f4c637a23222b3d530f61d19b55c753
Rachel Lewis <RLewis11769@gmail.com>
2021-06-02 20:08:16 +0000

    Remember to check semistandard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 /* Print string statements argv[1] number of times */
 const args = process.argv.slice(2);
-let num = args[0]
+const num = args[0];
+let x;
 
 if (num === 0) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 }
 for (x = 0; x < num; x++) {
   console.log('C is fun');
