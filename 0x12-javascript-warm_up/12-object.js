a89f9ba92ac304b26bb9590b0ccfdf7f675bb2b6
Rachel Lewis <RLewis11769@gmail.com>
2021-06-03 01:36:19 +0000

    Add data to 12-object.js

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* Change 12 to 89 */
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+/* My code starts here */
+myObject.value = 89;
+/* My code ends here */
+
+console.log(myObject);
