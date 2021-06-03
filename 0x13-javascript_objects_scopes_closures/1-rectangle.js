3d44adb2acfa476eba22864cc0f538ed93baca14
Rachel Lewis <RLewis11769@gmail.com>
2021-06-03 20:47:23 +0000

    Add data to 1-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+class Rectangle {
+/* Defines methods and attributes of Rectangle class */
+
+  constructor (w, h) {
+    /* Constructor function to add attributes to given Rectangle */
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
