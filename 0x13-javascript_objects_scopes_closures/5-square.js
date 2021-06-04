5f0c7945fe4d60deb11e5f52e549b95770217e75
Rachel Lewis <RLewis11769@gmail.com>
2021-06-04 01:28:37 +0000

    Data validation already in Rectangle so don't need to redo

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -7,10 +7,8 @@ class Square extends Rectangle {
 /* Defines methods and attributes of Square class that inherits from Rectangle */
 
   constructor (size) {
-    /* Constructor function to validate data and create new instance of Square */
-    if (size > 0) {
-      super(size, size);
-    }
+    /* Constructor function to create new instance of Square through Rectangle attributes and methods */
+    super(size, size);
   }
 }
 
