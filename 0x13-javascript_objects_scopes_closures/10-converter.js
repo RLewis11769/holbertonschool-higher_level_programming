4c86897decef997a1f2ae2fd36b6e2b2b87ae1a3
Rachel Lewis <RLewis11769@gmail.com>
2021-06-04 03:03:53 +0000

    Add comments, remove 'padding by blank lines'

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-  /* Returns number in base passed in */
+  /* Returns return of myConverter function */
+
   function myConverter (num) {
+    /* Returns num converted to base */
     return num.toString(base);
   }
   return myConverter;
-
 };
