66a36eead90205da6022098650dfc04b3664ad5c
Rachel Lewis <RLewis11769@gmail.com>
2021-06-04 01:54:57 +0000

    Inherits from 5-square.js for some reason

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 /* Imports Rectangle class */
-const Rectangle = require('./4-rectangle');
+const Square5 = require('./5-square');
 
-class Square extends Rectangle {
-/* Defines methods and attributes of Square class that inherits from Rectangle */
+class Square extends Square5 {
+/* Defines methods and attributes of Square class that inherits from Square in 5-square.js that inherits from Rectangle */
 
   constructor (size) {
     /* Constructor function to create new instance of Square through Rectangle attributes and methods */
