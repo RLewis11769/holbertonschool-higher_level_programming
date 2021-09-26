1422bfe4fd62c090673aef52c2df02ee26636d35
Rachel Lewis <RLewis11769@gmail.com>
2021-09-26 16:03:52 +0000

    Could have sworn this worked - but fixed to if/else

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,4 +1,8 @@
 // Toggles between red and green class on click DIV#toggle_header
 $('DIV#toggle_header').click(function () {
-  $('header').addClass('red green');
+  if ($("header").hasClass("red")) {
+    $("header").toggleClass("green");
+  } else {
+    $("header").toggleClass("red");
+  }
 });
