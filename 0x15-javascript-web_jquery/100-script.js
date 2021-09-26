8b826545535c9e320848f6740a94af178ffdee12
Rachel Lewis <RLewis11769@gmail.com>
2021-09-26 16:21:35 +0000

    Add data to 100-script - two options if doc loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// Change text color of header element
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('header').style.color = '#FF0000';
+  // document.querySelector('header').style = 'color: #FF0000';
+});
